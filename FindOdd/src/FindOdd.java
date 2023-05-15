13cd6357d45872a53b85b333f17aa3360078b37e
Aspink <aspink1@wp.pl>
2023-05-15 20:56:11 +0200

    FindOdd#04 FindOdd class constructed.

--- FindOdd/src/FindOdd.java
+++ FindOdd/src/FindOdd.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class FindOdd {
     public static int findIt(int[] a) {
+        Map<Integer, Long> countsMap = Arrays.stream(a)
+                .boxed()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        int odd;
+        for (Map.Entry<Integer, Long> entry : countsMap.entrySet()) {
+            if (entry.getValue() % 2 != 0) {
+                odd = entry.getKey();
+            }
+            break;
+        }
         return odd;
     }
 }

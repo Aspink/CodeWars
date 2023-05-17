1ae017cdfc6ad9329406302ee6f9dc82b508c8ea
Aspink <aspink1@wp.pl>
2023-05-17 21:12:50 +0200

    Quadrants#04 Method quadrant with table created.

--- Quadrants/src/Kata.java
+++ Quadrants/src/Kata.java
@@ -1,7 +1,9 @@
 public class Kata {
 
     public static int quadrant(int x, int y) {
-        int quadrant = 0;
-        return quadrant;
+        x = (int)(x / (2 * Math.abs(x)) + 0.5);
+        y = (int)(y / (2 * Math.abs(y)) + 0.5);
+        int[][] quadrant = new int[][]{{2, 1}, {3, 4}};
+        return quadrant[x][y];
     }
 }

614b4b8fa75e66154b0eb7ea8d5540c9b9c62524
Aspink <aspink1@wp.pl>
2023-05-18 12:25:20 +0200

    Quadrants#10 Method quadrant simplified.

--- Quadrants/src/Kata.java
+++ Quadrants/src/Kata.java
@@ -1,12 +1,7 @@
 public class Kata {
 
     public static int quadrant(int x, int y) {
-        int quadrant = 0;
-        if(x > 0) {
-            quadrant = (y > 0) ? 1 : 4;
-        } else {
-            quadrant = (y > 0) ? 2 : 3;
-        }
-        return quadrant;
+
+        return x > 0 ? y > 0? 1 : 4 : y > 0 ? 2 : 3;
     }
 }

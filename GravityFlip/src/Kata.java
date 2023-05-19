13361d61d1cb8b2dc893001c162f2c6b46477e69
Aspink <aspink1@wp.pl>
2023-05-19 22:22:21 +0200

    GravityFlip#03 Method flip started.

--- GravityFlip/src/Kata.java
+++ GravityFlip/src/Kata.java
@@ -1,2 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+import static java.util.Arrays.*;
+
 public class Kata {
+    public static int[] flip(char dir, int[] arr) {
+        ArrayList<Integer> array = new ArrayList<>(Collections.singletonList(arr));
+        array.sort();
+        if(dir == 'L') {
+            Collections.reverse(array);
+        }
+        for(int i = 0; i < array.size(); i++) {
+            arr[i] = array.get(i);
+        }
+        return arr;
+    }
 }
